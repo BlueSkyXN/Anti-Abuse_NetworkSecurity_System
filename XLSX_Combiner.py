367ef9df31e23087c2b0a2936180d3e1f21c48ed
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2024-03-22 16:13:11 +0800

    20240322-1613

---
+++ XLSX_Combiner.py
@@ -0,0 +1,27 @@
+import os
+import openpyxl
+from openpyxl.utils.dataframe import dataframe_to_rows
+import pandas as pd
+
+def merge_xlsx_files(output_file='Merged.xlsx'):
+    writer = pd.ExcelWriter(output_file, engine='openpyxl')
+
+    all_data = pd.DataFrame()
+
+    for filename in os.listdir('.'):
+        if filename.endswith('.xlsx') and not filename.startswith('~$'):
+            print(f'处理文件: {filename}')
+            df = pd.read_excel(filename)
+            all_data = pd.concat([all_data, df], ignore_index=True)
+
+            # 将原始表格数据复制到以文件名命名的新工作表中
+            df.to_excel(writer, sheet_name=os.path.splitext(filename)[0], index=False)
+
+    # 将合并后的数据写入到名为ALL的工作表中
+    all_data.to_excel(writer, sheet_name='ALL', index=False)
+
+    writer.save()
+    writer.close()
+
+if __name__ == "__main__":
+    merge_xlsx_files()
