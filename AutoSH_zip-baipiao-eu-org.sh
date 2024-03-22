3e247023882e5e09a0e3ed0c2b757f71551ca5a9
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2024-03-22 16:55:27 +0800

    20240322-1655

--- AutoSH_zip-baipiao-eu-org.sh
+++ AutoSH_zip-baipiao-eu-org.sh
@@ -9,18 +9,21 @@ HTTPS_PORTS=(443 2053 2083 2087 2096 8443)
 
 # 遍历文件夹中的所有txt文件
 for FILE in ${FOLDER_PATH}/*.txt; do
-  # 从文件名获取端口号
-  PORT=$(echo $(basename $FILE) | grep -o -E '[0-9]+' | tail -1)
-  SCHEMA="http" # 默认协议为HTTP
+    # 从文件名获取端口号
+    PORT=$(echo $(basename $FILE) | grep -o -E '[0-9]+' | tail -1)
+    SCHEMA="http" # 默认协议为HTTP
 
-  # 检查端口号是否在HTTPS端口数组中
-  if [[ " ${HTTPS_PORTS[@]} " =~ " ${PORT} " ]]; then
-    SCHEMA="https" # 如果是HTTPS端口，则修改协议为HTTPS
-  fi
+    # 检查端口号是否在HTTPS端口数组中
+    if [[ " ${HTTPS_PORTS[@]} " =~ " ${PORT} " ]]; then
+        SCHEMA="https" # 如果是HTTPS端口，则修改协议为HTTPS
+    fi
 
-  # 构建输出文件名
-  OUTPUT_FILE="${FOLDER_PATH}/$(basename $FILE .txt).xlsx"
+    # 构建输出文件名
+    OUTPUT_FILE="${FOLDER_PATH}/$(basename $FILE .txt).xlsx"
 
-  # 运行URL Availability Checker
-  python URL_Availability_Checker.py --input "$FILE" --output "$OUTPUT_FILE" --port $PORT --schema $SCHEMA > log/"$FILE".log
+    # 构建日志文件名
+    LOG_FILE="/home/runner/work/Anti-Abuse_NetworkSecurity_System/Anti-Abuse_NetworkSecurity_System/log/$(basename $FILE .txt).log"
+
+    # 运行URL Availability Checker
+    python URL_Availability_Checker.py --input "$FILE" --output "$OUTPUT_FILE" --port $PORT --schema $SCHEMA >> $LOG_FILE
 done
